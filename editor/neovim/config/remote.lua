1656f92e3b584646c048a12cc5da6b5d5fe9e512
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-12-10 06:06:40 +0600

    docs: remove verbosity from docs

--- editor/neovim/config/remote.lua
+++ editor/neovim/config/remote.lua
@@ -1,17 +1,17 @@
 -- ~/.config/nvim/lua/plugins/zx.lua
 return {
     "nurulhudaapon/zx",
-    dependencies = { 
-        "nvim-treesitter/nvim-treesitter",
-        "neovim/nvim-lspconfig",  -- for LSP support
-        "nvim-tree/nvim-web-devicons",  -- for file icons
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "neovim/nvim-lspconfig",           -- for LSP support
+      "nvim-tree/nvim-web-devicons",     -- for file icons
     },
     -- Load on startup instead of lazy-loading on filetype
     -- This ensures LSP and icons are ready immediately
     lazy = false,
-    priority = 50,  -- Load after lspconfig but early
+    priority = 50,   -- Load after lspconfig but early
     config = function()
-        vim.opt.runtimepath:prepend(vim.fn.stdpath("data") .. "/lazy/zx/editor/neovim")
-        vim.cmd("runtime! plugin/zx.lua")
+      vim.opt.runtimepath:prepend(vim.fn.stdpath("data") .. "/lazy/zx/editor/neovim")
+      vim.cmd("runtime! plugin/zx.lua")
     end,
-}
+  }
